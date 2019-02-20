7647de992f0f07e0cd6066f22d332416daf3ee9b
gxcad <gxcadr@gmail.com>
2019-02-19 20:52:33 -0800

    Markdown Previewer in react

---
+++ client/index.js
@@ -0,0 +1,5 @@
+import React from'react';
+import ReactDOM from'react-dom';
+import MarkdownPreview from './components/App.jsx';
+
+ReactDOM.render(<MarkdownPreview />, document.getElementById('root'));
